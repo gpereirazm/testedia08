aacb5e0cf92988dff35ede71b7720f7b5109d406
Guilherme Pereira da Silva Filho <guilherminhupereira07@gmail.com>
2025-10-08 08:30:19 -0300

    Create questão 4.py

---
+++ questão 4.py
@@ -0,0 +1,10 @@
+def ordenar_lista(numeros: list) -> list:
+ 
+  return sorted(numeros)
+
+lista = [20, 21, 4, 13, 7, 25, 23]
+resultado = ordenar_lista(lista)
+
+print(resultado)
+
+print(f"Lista original após a função: {lista}") 
